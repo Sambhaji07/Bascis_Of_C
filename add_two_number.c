120c503b576fecf3362462a68aa364aaf4fae546
Sambhaji Mitkar <85587206+Sambhaji07@users.noreply.github.com>
2024-03-10 00:52:16 +0530

    Add files via upload

---
+++ add_two_number.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+    int iNum1=5, iNum2=50, iSum=0;
+
+    iSum=iNum1+iNum2;
+    printf("Addition od two number is : %d",iSum);
+    return 0;
+}
+
+
+/*
+PS D:\Code Ranbhumi\C\Bascis_Of_C> gcc  add_two_number.c -o add_two_number
+PS D:\Code Ranbhumi\C\Bascis_Of_C> ./add_two_number
+Addition od two number is : 55
+PS D:\Code Ranbhumi\C\Bascis_Of_C> 
+*/
---
+++ first_program.c
@@ -0,0 +1,15 @@
+//  Write your first  program to print Jay Ganesh
+#include<stdio.h>
+
+int main()
+{
+    printf("Jay Ganesh");
+    return 0;
+}
+/*
+#                                                  Run and Execute 
+PS D:\Code Ranbhumi\C> gcc  first_program.c -o first_program
+PS D:\Code Ranbhumi\C> ./first_program
+Jay Ganesh
+PS D:\Code Ranbhumi\C> 
+*/
